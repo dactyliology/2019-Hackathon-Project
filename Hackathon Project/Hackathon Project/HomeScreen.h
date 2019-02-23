875291b8c35169da3530aeb0daefcb5379e08c45
Nicolette <43937352+NikiJD@users.noreply.github.com>
2019-02-23 12:15:29 -0800

    Made HomeScreen class

---
+++ Hackathon Project/Hackathon Project/HomeScreen.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Screen.h"
+
+class HomeScreen : public Screen
+{
+public:
+	void loadContent();
+
+};
--- Hackathon Project/Hackathon Project/Screen.h
+++ Hackathon Project/Hackathon Project/Screen.h
@@ -12,13 +12,13 @@ public:
 	virtual void loadContent() = 0; //made this pure virtual so we can override it in future 
 
 	// If there's anything that needs to be saved/destroyed/etc called here.
-	virtual void unloadContent();
+	//virtual void unloadContent();
 
 	// Moves things around, colors things, etc based on a passed in event.
 	virtual void update(sf::RenderWindow &window, sf::Event event);
 
 	// Does the drawing to the screen.
-	virtual void draw(sf::RenderWindow &window);
+	virtual void draw(sf::RenderWindow &window) = 0;
 
 protected:
 	sf::Texture backgroundTexture; // image for background
