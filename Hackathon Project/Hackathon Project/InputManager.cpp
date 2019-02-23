d7446a29a4be24e754a982da9a5de2d6bb1c8b66
Dlu20 <Dlu20@LAPTOP-E7LO495E>
2019-02-23 14:01:55 -0800

    Made mouseHover more simple

--- Hackathon Project/Hackathon Project/InputManager.cpp
+++ Hackathon Project/Hackathon Project/InputManager.cpp
@@ -12,18 +12,10 @@ bool InputManager::mouseClick(sf::Mouse::Button button)
 
 bool InputManager::mouseHover(sf::Sprite &object, sf::RenderWindow &window)
 {
-	/*sf::Vector2f mouseWorldPos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+	sf::Vector2f mouseWorldPos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 	sf::FloatRect globalBounds = object.getGlobalBounds();
 
-	return globalBounds.contains(mouseWorldPos);*/
-
-	if (sf::Mouse::getPosition(window).x > object.getPosition().x
-		&& sf::Mouse::getPosition(window).x < object.getPosition().x + object.getGlobalBounds().width
-		&& sf::Mouse::getPosition(window).y > object.getPosition().y
-		&& sf::Mouse::getPosition(window).y < object.getPosition().y + object.getGlobalBounds().height)
-
-		return true;
-	return false;
+	return globalBounds.contains(mouseWorldPos);
 }
 
 bool InputManager::mouseScroll(sf::Mouse::Wheel wheel)
